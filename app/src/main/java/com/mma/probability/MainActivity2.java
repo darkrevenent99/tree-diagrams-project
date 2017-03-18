048bb0c7cd14c547045ad45272e108eded8d4301
darkrevenent99 <kikiaung123@gmail.com>
2017-03-18 19:15:16 +0630

    Added Version 0.3 (First Commit)
    This is the first time uploading app files (sources of version 0.3).

---
+++ app/src/main/java/com/mma/probability/MainActivity2.java
@@ -0,0 +1,14 @@
+package com.mma.probability;
+import android.app.*;
+import android.os.*;
+
+public class MainActivity2 extends Activity
+{
+	@Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main2);
+		}
+	
+}
