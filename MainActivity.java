ff8864ce80f4e2e0ab7a37c5d0c25c1341a2a00f
darkrevenent99 <kikiaung123@gmail.com>
2017-03-19 17:09:17 +0630

    Version 0.4 Code Update
    one file edited, MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,122 @@
+package com.mma.probability;
+
+import android.app.*;
+import android.content.*;
+import android.content.res.*;
+import android.net.*;
+import android.os.*;
+import android.util.*;
+import android.view.*;
+import android.view.View.*;
+import android.widget.*;
+import java.io.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+		Button startBtn = (Button) findViewById(R.id.startBtn);
+		ImageView shareBtn = (ImageView) findViewById(R.id.shareBtn);
+		final String FOLDERPATH = "/sdcard/Android/data/com.mma.probability/TreeDiagrams";
+		shareBtn.setOnClickListener(new OnClickListener(){
+				@Override
+				public void onClick(View v) {
+					
+					Intent i = new Intent(MainActivity.this, MainActivity2.class);
+					startActivity(i);
+					
+					}
+		});
+		
+		startBtn.setOnClickListener( new OnClickListener() {
+
+				@Override
+				public void onClick(View v) {
+					
+				 	File dir = new File(FOLDERPATH);
+					File dirfile = new File(FOLDERPATH + "/index.html");
+					//File dir = getExternalCacheDir();
+					if(!dirfile.exists())
+						{
+						if(!dir.exists()){
+						dir.mkdir();}
+						copyAssetFolder(getAssets(), "Tree Diagrams", 
+											FOLDERPATH);
+						}
+						//copyAssetFolder(getAssets(), "Tree Diagrams", 
+						//								dir.getAbsolutePath());
+						//				}
+					
+			
+				  	File file = new File(FOLDERPATH + "/index.html") ;
+					//Uri uri = Uri.parse("intent:///storage/emulated/0/Tree%20Diagrams/index.html#Intent;scheme=file;type=*/*;launchFlags=0x10000000;B.by_open_as=true;end");//"file:///sdcard/Tree Diagrams/index.html") ;
+					openInBrowser(file);
+					//Intent i = new Intent(Intent.ACTION_VIEW);
+				 	//i.setDataAndType(Uri.fromFile(file), "*/*");
+					//i.setClassName("com.android.chrome", "android.intent.action.VIEW");
+					//startActivity(i);
+				}
+			});
+		
+	}
+	
+	private void openInBrowser(File file) {
+			final Intent browserIntent = new Intent(Intent.ACTION_VIEW);
+			browserIntent.setDataAndType(Uri.fromFile(file), "*/*");
+			startActivity(browserIntent);
+	}
+	
+	private static boolean copyAssetFolder(AssetManager assetManager,
+										   String fromAssetPath, String toPath) {
+        try {
+            String[] files = assetManager.list(fromAssetPath);
+            new File(toPath).mkdirs();
+            boolean res = true;
+            for (String file : files)
+                if (file.contains("."))
+                    res &= copyAsset(assetManager, 
+									 fromAssetPath + "/" + file,
+									 toPath + "/" + file);
+                else 
+                    res &= copyAssetFolder(assetManager, 
+										   fromAssetPath + "/" + file,
+										   toPath + "/" + file);
+            return res;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private static boolean copyAsset(AssetManager assetManager,
+									 String fromAssetPath, String toPath) {
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+			in = assetManager.open(fromAssetPath);
+			new File(toPath).createNewFile();
+			out = new FileOutputStream(toPath);
+			copyFile(in, out);
+			in.close();
+			in = null;
+			out.flush();
+			out.close();
+			out = null;
+			return true;
+        } catch(Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private static void copyFile(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+        while((read = in.read(buffer)) != -1){
+			out.write(buffer, 0, read);
+        }
+    }
+}
